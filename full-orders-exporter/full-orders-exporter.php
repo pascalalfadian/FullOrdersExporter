5ec8b1a0a1bd273d42df7002fa03aa6ba4e3e1b2
Pascal Alfadian Nugroho <pascal@dnartworks.co.id>
2020-04-08 10:17:42 +0700

    Initial commit, plugin boilerplate

---
+++ full-orders-exporter/full-orders-exporter.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Full Orders Exporter
+ * Description: WooCommerce Plugin to export orders, complete with their order meta and item meta.
+ * Version: 1.0
+ * Author: Pascal Alfadian Nugroho
+ * Author URI: https://informatika.unpar.ac.id/dosen/pascal
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: full-orders-exporter
+ */
